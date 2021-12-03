32e2942f853b106b0eaf16f34862495f73d09086
nickolasbmm <nickolasbmm44@gmail.com>
2021-12-03 18:32:38 -0300

    editar e deletar

--- Restaurant.sol
+++ Restaurant.sol
@@ -1,7 +1,5 @@
-// pragma solidity ^0.4.2;
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.10;
-// pragma experimental ABIEncoderV2;
 
 contract Delivery {
     
@@ -13,6 +11,7 @@ contract Delivery {
         string description;
         uint price;
         string imageURL;
+        bool exists;
     }
 
     struct Restaurant {
@@ -28,38 +27,44 @@ contract Delivery {
     mapping(uint => Restaurant) public restaurants;
     mapping(uint => MenuItem) public menuItens;
 
-    uint public restaurantsCount = 0;
-    uint public menuItensCount = 0;
+    uint public restaurantsCount = 0; //counts total restaurants 
+    uint public menuItensCount = 0; // counts total menuItens
+    uint public restaurantsCountExists = 0; //counts only restaurants that exist (i.e. not deleted)
 
 
     function addRestaurant (string memory _name,string memory _location, string memory _description,string memory _url) public {
-    restaurants[restaurantsCount] = Restaurant(restaurantsCount, _name, _location,_description,_url,0,true);
-    restaurantsCount++;
+        restaurants[restaurantsCount] = Restaurant(restaurantsCount, _name, _location,_description,_url,0,true);
+        restaurantsCount++;
+        restaurantsCountExists++;
     }
 
     function addMenuItem (uint _restaurantId,string memory _name,string memory _description,uint _price, string memory _url) public {
-    require(restaurants[_restaurantId].exists);
-    menuItens[menuItensCount] = MenuItem(menuItensCount, _restaurantId, _name, _description, _price, _url);
-    menuItensCount++;
-    restaurants[_restaurantId].menuCount++;
+        require(restaurants[_restaurantId].exists);
+        menuItens[menuItensCount] = MenuItem(menuItensCount, _restaurantId, _name, _description, _price, _url,true);
+        menuItensCount++;
+        restaurants[_restaurantId].menuCount++;
     }
 
     function getRestaurants() public view returns (Restaurant[] memory){
 
-        Restaurant[] memory itens = new Restaurant[](restaurantsCount);
+        Restaurant[] memory itens = new Restaurant[](restaurantsCountExists);
+        uint counter = 0;
         for(uint i=0;i<restaurantsCount;i++){
-            itens[i] = restaurants[i];
+            if(restaurants[i].exists){
+                itens[counter] = restaurants[i];
+                counter++;
+            }
         }
         return itens;
 
     }
 
     function getRestaurantMenu (uint _restaurantId) public view returns (MenuItem[] memory){
-
+        require(restaurants[_restaurantId].exists);
         MenuItem[] memory itens = new MenuItem[](restaurants[_restaurantId].menuCount);
         uint counter = 0;
         for(uint i=0;i<menuItensCount;i++){
-            if(menuItens[i].restaurantId == _restaurantId){
+            if(menuItens[i].exists && menuItens[i].restaurantId == _restaurantId){
                 itens[counter] = menuItens[i];
                 counter++;
             }
@@ -67,6 +72,41 @@ contract Delivery {
         return itens;
     
 
+    }
+
+    function editRestaurant(uint _restaurantId,string memory _name,string memory _location, string memory _description,string memory _url) public{
+        require(restaurants[_restaurantId].exists);
+        restaurants[_restaurantId].name = _name;
+        restaurants[_restaurantId].location = _location;
+        restaurants[_restaurantId].description = _description;
+        restaurants[_restaurantId].imageURL = _url;
+    }
+
+    function editMenuItem(uint _menuItemId,string memory _name,uint _price, string memory _url) public {
+        require( menuItens[_menuItemId].exists);
+        menuItens[_menuItemId].name = _name;
+        menuItens[_menuItemId].price = _price;
+        menuItens[_menuItemId].imageURL = _url;
+    }
+
+    function deleteRestaurant(uint _restaurantId) public{
+        require(restaurants[_restaurantId].exists);
+        restaurantsCountExists--;      
+        // Delete restaurant menu
+        for(uint i=0;i<menuItensCount;i++){
+            if(menuItens[i].exists && menuItens[i].restaurantId == _restaurantId){
+                deleteMenuItem(i);
+            }
+        }
+
+        restaurants[_restaurantId].exists = false;
+    }
+
+    function deleteMenuItem(uint _menuItemId) public {
+        require(menuItens[_menuItemId].exists);
+        require(restaurants[menuItens[_menuItemId].restaurantId].exists);
+        restaurants[menuItens[_menuItemId].restaurantId].menuCount--;
+        menuItens[_menuItemId].exists = false;
     }
 
 } 
